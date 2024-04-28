33db9e58ad755c5dc4da6e81f520f8261b573725
Ethan <ethan@ethanmilon.dev>
2024-04-28 15:01:39 +0200

    ref: move CollisionGrid and vec2

---
+++ src/collisiongrid.h
@@ -0,0 +1,196 @@
+#include <cmath>
+#include <cstddef>
+#include <vector>
+#include <span>
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T> class Grid {
+    private:
+	std::vector<T> data;
+	std::size_t width, height;
+
+    public:
+	Grid(std::size_t width, std::size_t height)
+		: data(width * height)
+		, width(width)
+		, height(height)
+	{
+	}
+
+	Grid(std::size_t width, std::size_t height, const T &value)
+		: data(width * height, value)
+		, width(width)
+		, height(height)
+	{
+	}
+
+	T &operator()(std::size_t x, std::size_t y)
+	{
+		return data[y * width + x];
+	}
+
+	const T &operator()(std::size_t x, std::size_t y) const
+	{
+		return data[y * width + x];
+	}
+
+	T &at(std::size_t x, std::size_t y)
+	{
+		if (x < 0 || x >= width || y < 0 || y >= height) {
+			throw std::out_of_range("Grid::at");
+		}
+		return data[y * width + x];
+	}
+
+	const T &at(std::size_t x, std::size_t y) const
+	{
+		if (x < 0 || x >= width || y < 0 || y >= height) {
+			throw std::out_of_range("Grid::at");
+		}
+		return data[y * width + x];
+	}
+
+	std::span<T> flatten()
+	{
+		return { data.begin(), data.size() };
+	}
+
+	std::span<const T> flatten() const
+	{
+		return { data.begin(), data.size() };
+	}
+
+	auto begin() const
+	{
+		return data.begin();
+	}
+
+	auto end() const
+	{
+		return data.end();
+	}
+
+	auto cbegin() const
+	{
+		return data.cbegin();
+	}
+
+	auto cend() const
+	{
+		return data.cend();
+	}
+
+	std::pair<std::size_t, std::size_t> size() const
+	{
+		return { width, height };
+	}
+};
+
+class CollisionGrid {
+    private:
+	struct Cell {
+		uint cycle;
+		std::vector<uint> ids;
+	};
+
+	uint cycle = 0;
+	float cell_size;
+
+	Grid<Cell> grid;
+
+    public:
+	CollisionGrid(float width, float height, float cell_size)
+		: cell_size(cell_size)
+		, grid(width / cell_size, height / cell_size, { cycle, {} })
+	{
+	}
+
+	void clear()
+	{
+		cycle++;
+	}
+
+	void addObject(float minX, float minY, float maxX, float maxY, uint id);
+
+	std::vector<uint> getCollisions(float minX, float minY, float maxX, float maxY) const;
+
+	std::vector<std::pair<uint, uint> > getAllCollisions() const;
+};
+template <typename T> void remove_duplicates(std::vector<T> &v)
+{
+	std::sort(v.begin(), v.end());
+	auto it = std::unique(v.begin(), v.end());
+	v.erase(it, v.end());
+}
+
+inline void CollisionGrid::addObject(float minX, float minY, float maxX, float maxY, uint id)
+{
+	size_t width = grid.size().first;
+	size_t height = grid.size().second;
+
+	size_t x0 = std::clamp<size_t>(std::floor(minX / cell_size), 0, width - 1);
+	size_t y0 = std::clamp<size_t>(std::floor(minY / cell_size), 0, height - 1);
+	size_t x1 = std::clamp<size_t>(std::ceil(maxX / cell_size), 0, width - 1);
+	size_t y1 = std::clamp<size_t>(std::ceil(maxY / cell_size), 0, height - 1);
+
+	for (size_t y = y0; y <= y1; y++) {
+		for (size_t x = x0; x <= x1; x++) {
+			auto &cell = grid(x, y);
+
+			if (cycle != cell.cycle) {
+				cell.cycle = cycle;
+				cell.ids.clear();
+			}
+			cell.ids.push_back(id);
+		}
+	}
+}
+
+inline std::vector<uint> CollisionGrid::getCollisions(float minX, float minY, float maxX, float maxY) const
+{
+	std::vector<uint> result;
+
+	size_t width = grid.size().first;
+	size_t height = grid.size().second;
+
+	size_t x0 = std::clamp<size_t>(std::floor(minX / cell_size), 0, width - 1);
+	size_t y0 = std::clamp<size_t>(std::floor(minY / cell_size), 0, height - 1);
+	size_t x1 = std::clamp<size_t>(std::ceil(maxX / cell_size), 0, width - 1);
+	size_t y1 = std::clamp<size_t>(std::ceil(maxY / cell_size), 0, height - 1);
+
+	for (size_t y = y0; y <= y1; y++) {
+		for (size_t x = x0; x <= x1; x++) {
+			const auto &cell = grid(x, y);
+			if (cycle != cell.cycle) {
+				continue;
+			}
+
+			result.insert(result.end(), cell.ids.begin(), cell.ids.end());
+		}
+	}
+
+	remove_duplicates(result);
+
+	return result;
+}
+
+inline std::vector<std::pair<uint, uint> > CollisionGrid::getAllCollisions() const
+{
+	std::vector<std::pair<uint, uint> > collisions;
+
+	for (const auto &cell : grid) {
+		if (cycle != cell.cycle || cell.ids.size() < 2) {
+			continue;
+		}
+		for (auto it = cell.ids.begin(); it != cell.ids.end(); it++) {
+			for (auto jt = it + 1; jt != cell.ids.end(); jt++) {
+				collisions.push_back({ *it, *jt });
+			}
+		}
+	}
+
+	remove_duplicates(collisions);
+
+	return collisions;
+}
---
+++ src/vec2.h
@@ -0,0 +1,111 @@
+#include <cmath>
+#include <ostream>
+
+template <typename T = float> class vec2 {
+    public:
+	T x, y;
+
+	vec2 operator+(const vec2 &v) const
+	{
+		return { x + v.x, y + v.y };
+	}
+
+	vec2 operator-(const vec2 &v) const
+	{
+		return { x - v.x, y - v.y };
+	}
+
+	vec2 operator*(T s) const
+	{
+		return { x * s, y * s };
+	}
+
+	friend vec2 operator*(T s, const vec2 v)
+	{
+		return v * s;
+	}
+
+	vec2 operator/(T s) const
+	{
+		return { x / s, y / s };
+	}
+
+	vec2 &operator+=(const vec2 &v)
+	{
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+
+	vec2 &operator-=(const vec2 &v)
+	{
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}
+
+	vec2 &operator*=(T s)
+	{
+		x *= s;
+		y *= s;
+		return *this;
+	}
+	vec2 &operator/=(T s)
+	{
+		x /= s;
+		y /= s;
+		return *this;
+	}
+
+	vec2 operator-() const
+	{
+		return { -x, -y };
+	}
+	vec2 operator+() const
+	{
+		return { x, y };
+	}
+
+	vec2 normalized() const
+	{
+		return { x / norm(), y / norm() };
+	}
+
+	T norm() const
+	{
+		return std::sqrt(x * x + y * y);
+	}
+
+	T dot(const vec2 &v) const
+	{
+		return x * v.x + y * v.y;
+	}
+	vec2 rotate(T angle) const
+	{
+		float s = std::sin(angle);
+		T c = std::cos(angle);
+		return { x * c - y * s, x * s + y * c };
+	}
+
+	vec2 rotate90CCW() const
+	{
+		return { -y, x };
+	}
+
+	vec2 rotate90CW() const
+	{
+		return { y, x };
+	}
+
+	bool isnan() const
+	{
+		return std::isnan(x) || std::isnan(y);
+	}
+
+	friend std::ostream &operator<<(std::ostream &os, const vec2 &v)
+	{
+		return os << "(" << v.x << ", " << v.y << ")";
+	}
+};
+
+using vec2f = vec2<float>;
